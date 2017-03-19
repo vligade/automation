0cd894dc4bc1f0c9415bb3bac774eb3ed7be8e8b
Joni Kähärä <joni.kahara@gmail.com>
2017-03-19 20:36:31 +0200

    separate creation and retrieval

--- automation/handler.py
+++ automation/handler.py
@@ -1,22 +1,27 @@
 import json
 
-def hello(event, context):
+def retrieve_url(event, context):
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        'url': '...',
     }
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        'statusCode': 200,
+        'body': json.dumps(body)
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+def create_url(event, context):
+
+    body = {
+        'url': '...',
     }
-    """
+
+    response = {
+        'statusCode': 201,
+        'body': json.dumps(body)
+    }
+
+    return response
